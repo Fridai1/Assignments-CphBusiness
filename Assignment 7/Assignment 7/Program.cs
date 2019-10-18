c725551e0c063374e73da5e3688510dc1370fa5f
Nikolai <nikolaiviby3@gmail.com>
2019-10-18 12:25:34 +0200

    initial

---
+++ Assignment 7/Assignment 7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("you current balance: " + bank.Balance);
+                Console.WriteLine("deposit or withdraw?");
+                string answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "deposit":
+                    {
+                        Console.WriteLine("amount?");
+                        string amount = Console.ReadLine();
+                        double dAmount = double.Parse(amount);
+                        Console.WriteLine(bank.Deposit(dAmount));
+                        break;
+                    }
+                    case "withdraw":
+                    {
+                        Console.WriteLine("amount?");
+                        string amount = Console.ReadLine();
+                        double dAmount = double.Parse(amount);
+                        Console.WriteLine(bank.Withdraw(dAmount));
+                        break;
+                    }
+                }
+
+                
+            }
+        }
+    }
+}
---
+++ Assignment 7/Assignment 7/bank.cs
@@ -0,0 +1,31 @@
+#define CONTRACTS_FULL
+
+using System.Diagnostics.Contracts;
+
+namespace Assignment_7
+{
+    class Bank
+    {
+        private double _accountBalance = 10000;
+        public double Deposit(double amount)
+        {
+
+            Contract.Requires(amount > 0,"amount was 0 or less");
+            Contract.Ensures(_accountBalance == _accountBalance + amount,"the balance didn't match");
+            _accountBalance += amount;
+            return _accountBalance;
+
+        }
+
+        public double Withdraw(double amount)
+        {
+            Contract.Requires(amount > 0);
+            Contract.Requires(_accountBalance - amount > 0);
+            Contract.Ensures(_accountBalance == _accountBalance - amount);
+            _accountBalance -= amount;
+            return _accountBalance;
+        }
+
+        public double Balance { get => _accountBalance; }
+    }
+}
